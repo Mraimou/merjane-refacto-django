051b42783096cec39dd28a62b272a847f7099f7e
mraimou <elberriri.mariem@gmail.com>
2025-09-02 20:54:38 +0100

    process_order refactored and tests updated

--- orders/my_views.py
+++ orders/my_views.py
@@ -10,37 +10,98 @@ from .repositories.product_repository import pr
 from .repositories.order_repository import or_
 from .services.implementations.product_service import ps
 
+
+class OrderProcessor:
+    def __init__(self, order):
+        self.order = order
+
+    def process(self):
+        for product in self.order.get_items():
+            if product.type == "NORMAL":
+                self._process_normal(product)
+            elif product.type == "SEASONAL":
+                self._process_seasonal(product)
+            elif product.type == "EXPIRABLE":
+                self._process_expirable(product)
+
+    def _decrement(self, product):
+        product.available -= 1
+
+    def _save(self, product):
+        product.save()
+
+    def _process_normal(self, product):
+        if product.available > 0:
+            self._decrement(product)
+            self._save(product)
+        elif product.lead_time > 0:
+            ps.notify_delay(product.lead_time, product)
+
+    def _is_seasonal(self, product):
+        is_seasonal = product.season_start_date < date.today() < product.season_end_date
+        return product.available > 0 and is_seasonal
+
+    def _process_seasonal(self, product):
+        if self._is_seasonal(product):
+            self._decrement(product)
+            self._save(product)
+        else:
+            ps.handle_seasonal_product(product)
+
+    def _is_expirable(self, product):
+        return product.available > 0 and product.expiry_date > date.today()
+
+    def _process_expirable(self, product):
+        if self._is_expirable(product):
+            self._decrement(product)
+            self._save(product)
+        else:
+            ps.handle_expired_product(product)
+
+
 @csrf_exempt
 @require_POST
 def process_order(request, order_id):
-    o = or_.find_by_id(order_id).get()
-    print(o)
-    ids = []
-    ids.append(order_id)
-    products = o.get_items()
-    for p in products:
-        if p.type == "NORMAL":
-            if p.available > 0:
-                p.available = p.available - 1
-                pr.save(p)
-            else:
-                lead_time = p.lead_time
-                if lead_time > 0:
-                    ps.notify_delay(lead_time, p)
+    # This part should be refactored as well
+    order = or_.find_by_id(order_id).get()
 
-        elif p.type == "SEASONAL":
-            if (date.today() > p.season_start_date and date.today() < p.season_end_date and p.available > 0):
-                p.available = p.available - 1
-                pr.save(p)
-            else:
-                ps.handle_seasonal_product(p)
+    processor = OrderProcessor(order)
+    processor.process()
 
-        elif p.type == "EXPIRABLE":
-            if p.available > 0 and p.expiry_date > date.today():
-                p.available = p.available - 1
-                pr.save(p)
-            else:
-                ps.handle_expired_product(p)
+    response = ProcessOrderResponse(order.id)
+    return JsonResponse({"id": response.id}, status=200)
 
-    response = ProcessOrderResponse(o.id)
-    return JsonResponse({'id': response.id}, status=200)
+# @csrf_exempt
+# @require_POST
+# def process_order(request, order_id):
+#     o = or_.find_by_id(order_id).get()
+#     print(o)
+#     ids = []
+#     ids.append(order_id)
+#     products = o.get_items()
+#     for p in products:
+#         if p.type == "NORMAL":
+#             if p.available > 0:
+#                 p.available = p.available - 1
+#                 pr.save(p)
+#             else:
+#                 lead_time = p.lead_time
+#                 if lead_time > 0:
+#                     ps.notify_delay(lead_time, p)
+#
+#         elif p.type == "SEASONAL":
+#             if (date.today() > p.season_start_date and date.today() < p.season_end_date and p.available > 0):
+#                 p.available = p.available - 1
+#                 pr.save(p)
+#             else:
+#                 ps.handle_seasonal_product(p)
+#
+#         elif p.type == "EXPIRABLE":
+#             if p.available > 0 and p.expiry_date > date.today():
+#                 p.available = p.available - 1
+#                 pr.save(p)
+#             else:
+#                 ps.handle_expired_product(p)
+#
+#     response = ProcessOrderResponse(o.id)
+#     return JsonResponse({'id': response.id}, status=200)
--- orders/tests/test_my_view.py
+++ orders/tests/test_my_view.py
@@ -6,34 +6,53 @@ from django.urls import reverse
 
 from orders.entities.product import Product
 from orders.entities.order import Order
-
+from ..my_views import OrderProcessor
 
 class TestMyView(TestCase):
-    @patch('orders.my_views.ps')
-    def test_process_order_should_return(self, mock_ns):
+    @classmethod
+    def setUpTestData(cls):
+        # On crée une liste de produits + order une seule fois pour toute la classe de tests.
+        normal_product = Product(available=15, lead_time=30, type="NORMAL", name="USB Cable")
         products = [
-            Product(available=15, lead_time=30, type="NORMAL",    name="USB Cable"),
-            Product(available=10, lead_time=0,  type="NORMAL",    name="USB Dongle"),
+            normal_product,
+            Product(available=10, lead_time=0, type="NORMAL", name="USB Dongle"),
             Product(available=15, lead_time=30, type="EXPIRABLE", name="Butter",
                     expiry_date=date.today() + timedelta(days=26)),
-            Product(available=90, lead_time=6,  type="EXPIRABLE", name="Milk",
+            Product(available=90, lead_time=6, type="EXPIRABLE", name="Milk",
                     expiry_date=date.today() - timedelta(days=2)),
-            Product(available=15, lead_time=30, type="SEASONAL",  name="Watermelon",
+            Product(available=15, lead_time=30, type="SEASONAL", name="Watermelon",
                     season_start_date=date.today() - timedelta(days=2),
                     season_end_date=date.today() + timedelta(days=58)),
-            Product(available=15, lead_time=30, type="SEASONAL",  name="Grapes",
+            Product(available=15, lead_time=30, type="SEASONAL", name="Grapes",
                     season_start_date=date.today() + timedelta(days=180),
                     season_end_date=date.today() + timedelta(days=240)),
         ]
+        cls.normal_product = normal_product
         for p in products:
             p.save()
+        order = Order.objects.create()
+        order.products.set(products)
+        cls.order = order
 
-        o = Order.objects.create()
-        o.products.set(products)
-
-        url = reverse('process_order', args=[o.id])
+    @patch('orders.my_views.ps')
+    def test_process_order_should_return(self, mock_ns):
+        url = reverse('process_order', args=[self.order.id])
         response = self.client.post(url, content_type="application/json")
 
         self.assertEqual(response.status_code, 200)
-        result_order = Order.objects.get(id=o.id)
-        self.assertEqual(result_order.id, o.id)
+        result_order = Order.objects.get(id=self.order.id)
+        self.assertEqual(result_order.id, self.order.id)
+
+    # Seasonal items tests
+
+    # EXPIRABLE items tests
+
+    # NORMAL items tests
+    @patch('orders.my_views.ps')
+    def test_process_normal(self, mock_ns):
+        processor = OrderProcessor(self.order)
+        processor._process_normal(self.normal_product)
+        self.assertEqual(self.normal_product.available, 14)
+
+
+
